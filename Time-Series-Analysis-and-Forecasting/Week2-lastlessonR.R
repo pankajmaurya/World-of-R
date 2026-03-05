75bd3de874bf89fb1503b96dfd20c752cac04bd0
Pankaj Maurya <pankajmaurya@gmail.com>
2026-03-05 19:12:05 +0530

    PQ also done

--- Time-Series-Analysis-and-Forecasting/Week2-lastlessonR.R
+++ Time-Series-Analysis-and-Forecasting/Week2-lastlessonR.R
@@ -23,3 +23,17 @@ plot(ddata$seasonal)
 plot(ddata$random)
 
 #help(ts)
+
+trend = seq(10, 30, length = 100)
+X = 10 + trend + sin(10 * trend)
+
+tsdata2 <- ts(X, frequency=12)
+
+ddata2 <- decompose(tsdata2, "multiplicative")
+
+plot(ddata2)
+
+ddata3 <- decompose(tsdata2, "additive")
+
+plot(ddata3)
+
